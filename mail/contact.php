8fc0a4b35e148dd6c751e7ec7246a38a1428b80c
Accelerator93 <110353672+Accelerator93@users.noreply.github.com>
2023-10-05 09:56:59 -0600

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ if (isset($_POST['sendMessageButton'])){
     $header = "From: antonioholguin.terrazas@gmail.com" . "\r\n";
     $header .= "Reply-To: antonioholguin.terrazas@gmail.com" . "\r\n";
   $header .= "X-Mailer: PHP/" . phpversion();
-  $mail = @mail($email, $m_subject, $phone, $message, $header);
+  $mail = mail($email, $m_subject, $phone, $message, $header);
   $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nphone: $phone\n\nMessage: $message";
   if(!$mail)
   http_response_code(500);
