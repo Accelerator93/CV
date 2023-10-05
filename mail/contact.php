a456dfd4bbe83b6b0bff0dad499773e26302bc92
Accelerator93 <110353672+Accelerator93@users.noreply.github.com>
2023-10-05 09:49:52 -0600

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -1,21 +1,18 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if (isset($_POST['sendMessageButton'])){
+  if(!empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['phone']) && !empty($_POST['message']) && !empty($_POST['email'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $m_subject = $_POST['subject'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $header = "From: antonioholguin.terrazas@gmail.com" . "\r\n";
+    $header .= "Reply-To: antonioholguin.terrazas@gmail.com" . "\r\n";
+  $header .= "X-Mailer: PHP/" . phpversion();
+  $mail = @mail($email, $m_subject, $phone, $message, $header);
+  $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nphone: $phone\n\nMessage: $message";
+  if(!$mail)
   http_response_code(500);
-  exit();
+  }
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "antonioholguin.terrazas@gmail.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nphone: $phone\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
 ?>
